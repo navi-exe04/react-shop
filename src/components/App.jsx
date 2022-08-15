627c2c53e8222afc42e046ae7f8b9da0daa515fd
Raul Ivan <ra04navi@outlook.com>
2022-08-15 13:53:36 -0500

    Creación de los archivos del curso. Se ha visto el como agregar estilos y configurar el webpack para que el proyecto funcione correctamente

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/global.css'
+import Layout from '../containers/Layout';
+import Login from '../containers/Login';
+
+const App = () => {
+    return (
+        <Layout>
+            <Login/>
+        </Layout>
+    );
+}
+
+export default App;
---
+++ src/containers/Login.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../styles/Login.scss';
+
+const Login = () => {
+    return (
+        <div classname ="login">
+            <div classname ="form-container">
+                <img src="./logos/logo_yard_sale.svg" alt="logo" classname ="logo" />
+                <h1 classname ="title">Create a new password</h1>
+                <p classname ="subtitle">Enter a new passwrd for yue account</p>
+                <form action="/" classname ="form">
+                    <label htmlFor="password" classname ="label">Password</label>
+                    <input type="password" id="password" placeholder="*********" classname ="input input-password" />
+                    <label htmlFor="new-password" classname ="label">Password</label>
+                    <input type="password" id="new-password" placeholder="*********" classname ="input input-password" />
+                    <input type="submit" defaultValue="Confirm" classname ="primary-button login-button" />
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
